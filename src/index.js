31f26dbc3772bb1dcff1eab496a339b7e8221a4b
Rosmu <rosamari.sofia@gmail.com>
2025-05-05 01:03:48 +0300

    testing routes to the deployed version

--- src/index.js
+++ src/index.js
@@ -7,16 +7,27 @@ import exerciseRouter from './routes/exercise-router.js';
 import sicknessRouter from './routes/sickness-router.js';
 import othersRouter from './routes/others-router.js';
 import kubiosRouter from './routes/kubios-router.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { errorHandler, notFoundHandler } from './middlewares/error-handler.js';
 
 const hostname = '127.0.0.1';
-const app = express();
 const port = 3000;
+const app = express();
 
 app.use(cors());
 app.use('/', express.static('public'));
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// SPA fallback
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) return next(); // Älä ohita API-pyyntöjä
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api', express.static('docs'));
 
